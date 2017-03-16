a3d9d343a094ceeaf2522e31812f4a353d16f106
Li Yan <li.yan.ly414@gmail.com>
2017-03-16 17:04:11 -0400

    prepare bpsk and qpsk for cluster

---
+++ cluster_uva2/br/br_9.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 22:06:02 2017
+
+@author: misWin
+
+This is a template of python files for simulation algorithem on Rivanna
+"""
+
+from milp4 import *
+seed = 83709
+np.random.seed(seed)
+kwargs = {'logfile': 'br_9.log', 'mipgap': 0.001, 'miphint': True, 'threads': 4, 'mipfocus': 1, 'presolve': 2}
+
+batch_id = 9
+network_cost = pd.read_csv('nsf-24nodes.csv', header=None, index_col=None)
+network_cost = network_cost.as_matrix()
+sn = Network(network_cost, modulation='bpsk')
+demands_file = 'br_9.csv'
+demands = pd.read_csv(demands_file)
+
+iteration_history_tr, iteration_history_gn = sn.iterate(demands, **kwargs)
+
+iteration_history = (iteration_history_tr, iteration_history_gn)
+output_file = 'br_9.pkl'
+save_data(output_file, (sn, iteration_history))
